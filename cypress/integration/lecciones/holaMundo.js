6a3a95d6c7088a38b2d0ed3a0b50f70c770114d5
DESKTOP-U9QKDQR\Yoni <jonatancanteroatienza@gmail.com>
2023-12-09 20:04:44 +0100

    Proyecto creado

---
+++ cypress/integration/lecciones/holaMundo.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+describe("Este será nuestro hola mundo en cypress", ()=> {
+    it("Hola mundo", () => {
+        cy.log("Hola Mundo desde nuestro proyecto recien creado!");
+    });
+    it("Segundo test", () => {
+        cy.log("Hola Mundo desde nuestro proyecto recien creado!");
+    });
+    it("tercer test", () => {
+        cy.log("Hola Mundo desde nuestro proyecto recien creado!");
+    });
+    it("cuarto test", () => {
+        cy.log("Hola Mundo desde nuestro proyecto recien creado!");
+    });
+})
